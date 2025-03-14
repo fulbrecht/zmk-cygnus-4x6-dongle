6559a235232152b16e652542f9d7655ca14b3f5c
fulbrecht <13141050+fulbrecht@users.noreply.github.com>
2025-03-14 17:48:29 -0400

    Update custom_status_screen.c

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -39,16 +39,14 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
     
 // Bottom Left
-
-    zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
-
-// Bottom Right
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
     zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
     // lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -2);
-    lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+    lv_obj_align(zmk_widget_hid_indicators_obj(&hid_indicators_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
 #endif
+
+// Bottom Right
+
 
 // Top Right
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
@@ -56,8 +54,11 @@ lv_obj_t *zmk_display_status_screen() {
 
 // Top Left
 
-    zmk_widget_modifiers_init(&modifiers_widget, screen);
-    lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+
+    // zmk_widget_modifiers_init(&modifiers_widget, screen);
+    // lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 
 // Top Mid
     zmk_widget_layer_status_init(&layer_status_widget, screen);
