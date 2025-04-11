6ae0ae5882cfc913a462cae36bfd5a46bb6d9fcb
fulbrecht <13141050+fulbrecht@users.noreply.github.com>
2025-04-11 02:00:43 -0400

    Update custom_status_screen.c

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -47,7 +47,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 // Bottom Left
     zmk_widget_modifiers_init(&modifiers_widget, screen);
-    lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 10);
 
 // Top Right
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
@@ -61,9 +61,9 @@ lv_obj_t *zmk_display_status_screen() {
     // zmk_widget_modifiers_init(&modifiers_widget, screen);
     // lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 
-// Center
+// Top Mic
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_CENTER, 0, 0);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_MID, 0, 0);
     
 
     return screen;
